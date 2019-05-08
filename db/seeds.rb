e345d7eb1b362d297d32036a140a5415ca906ab7
Brian <GuitarRook94@gmail.com>
2019-05-08 00:13:27 -0400

    Added some extra seeds, and changed the users controller to avoid CORS conflicts when this server is ran locally

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
-User.create(name:"Brian", comment: "Test")
+User.create(name: "Brian", comment: "I made this game!");
+User.create(name: "Jake", comment: "Cool Site!");
+User.create(name: "Sarah", comment: "I knocked them all down");
+User.create(name: "Steve Jobs", comment: "How much for the site");
+User.create(name: "Gamer", comment: "This is really fun");
